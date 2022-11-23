9b26a5145d463199ac92c584826a1afb729f7f9f
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-11-23 19:54:54 +0600

    project initialized

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import '../src/assets/scss/style.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
