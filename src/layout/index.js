76239af1a5f9e0af63d8617cfc81ddc0a7b87619
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-11-25 05:14:33 +0600

    style updated

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/heading-has-content */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
