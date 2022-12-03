3c664132c6b1cf2d88a7db4a9d5c8ef624f8d691
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-12-04 03:19:40 +0600

    build updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "../src/assets/scss/app.scss";
-import Route from "./Routes/Routes";
+import AllRoutes from "./Routes/Routes";
 
 function App() {
   return (
     <React.Fragment>
-      <Route />
+      <AllRoutes />
     </React.Fragment>
   );
 }
